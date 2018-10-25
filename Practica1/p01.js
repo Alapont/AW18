61aaa0dd179cf62bbfbbb6cb8a3320274307ad74
Nerea <jguillen.nereajimenez@gmail.com>
2018-10-25 17:31:43 +0200

    más funciones

--- Practica1/p01.js
+++ Practica1/p01.js
@@ -1,14 +1,32 @@
 'use strict';
 
 let listaTareas =[
-    {text:"Preparar práxtica AW", tags: ["AW", "practica"]},
+    {text:"Preparar práctica AW", tags: ["AW", "practica"]},
     {text: "Mirar fechas congreso", done:true, tags:[]},
     {text: "Ir al supermercado", tags:["personal"]},
     {text: "Mudanza", tags:["personal"]},
 ];
 
 function getToDoTasks(tasks){
-    var toDo=tasks.splice();
-
-    return toDo.filter(done=true);
+    return tasks.filter(task=>task.done!=true).map(task =>task.text);
 }
+
+function findByTag(tasks, tag){
+    return tasks.filter(task=>task.tags.includes(tag));
+}
+
+function findByTags(tasks, par){
+    return tasks.filter(
+        task=>task.tags.some(
+            tag=>par.some(
+                x=>x==tag)));
+}
+
+function countDone(tasks){
+    return tasks.filter(task=>task.done==true).length;
+}
+
+function createTask(texto){
+    
+}
+console.log(countDone(listaTareas));
