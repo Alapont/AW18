13f5024c277c173f6da377c379e35916ffb2bec3
Alapont <pablodragon@gmail.com>
2018-11-08 12:47:31 +0100

    me falta haser el colbak

--- Hoja A (node)/ej2.js
+++ Hoja A (node)/ej2.js
@@ -27,7 +27,7 @@ class DAO {
                         console.log(`ERROR AL OBTENER LA CONEXION: ${err.message}`);
                     } else {
                         usuario.id = resultado.insertId;
-                        console.log("EVERYTHING IS AWESOME");
+                        console.log(`Usuario insertado con id: ${resultado.insertid}`);
                     }
                 })
             }
@@ -40,31 +40,87 @@ class DAO {
             if (err) {
                 console.log(`ERROR AL OBTENER LA CONEXION: ${err.message}`);
             } else {
+                //Deberíamos ver si el usuario existe
                 const sql = "INSERT INTO `mensajes` (`id`, `idOrigen`, `idDestino`, `mensaje`, `hora`, `leido`) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP, '0');";
                 connection.query(sql, [usuarioOrigen.id, usuarioDestino.id, mensaje], function (err, resultado) {
                     if (err) {
                         console.log(`ERROR AL OBTENER LA CONEXION: ${err.message}`);
                     } else {
-                        console.log(`EVERYTHING IS AWESOMEx2:${resultado.insertId}`);
+                        console.log(`Mensaje enviado :${resultado.insertId}`);
                     }
                 })
             }
             connection.release();
         });
     }
+
+    bandejaEntrada(usuario,callback) {
+        this._pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(`ERROR AL OBTENER LA CONEXION: ${err.message}`);
+            } else {
+                const sql = "Select DISTINCT mensaje "+
+                "FROM mensajes "+
+                "WHERE idDestino = ? AND leido = 0";
+                connection.query(sql, [usuario.id], function (err, resultado) {
+                    if (err) {
+                        console.log(`ERROR AL OBTENER LA CONEXION: ${err.message}`);
+                    } else {
+                        resultado.forEach(mensaje => {
+                            console.log(`${mensaje.mensaje}`)
+                        });
+                    }
+                })
+            }
+            connection.release();
+        });
+    }
+    buscarUsuario(str, callback){
+        this._pool.getConnection(function (err, connection) {
+            if (err) {
+                console.log(`ERROR AL OBTENER LA CONEXION: ${err.message}`);
+            } else {
+                const sql = "Select * FROM usuarios WHERE nombre LIKE ?";
+                connection.query(sql, ["%"+str+"%"], function (err, resultado) {//Se ponen las wildcards ahí antes de que lo escape
+                    if (err) {
+                        console.log(`ERROR AL OBTENER LA CONEXION: ${err.message}`);
+                    } else {
+                        console.log(`La búsqueda '${str}' resultó en ${resultado.length}:`)
+                        resultado.forEach(element => {
+                            console.log(`${element.nombre}`)
+                        });
+                    }
+                })
+            }
+            connection.release();
+        });
+    }
+
+    
 }
 module.exports = DAO;
 
 console.log("creacion de dao");
 let d = new DAO("localhost", "root", "", "ej2");
 
-console.log("insertamos usuario");
+//console.log("insertamos usuario");
 let pablo = {
     nombre: "pont",
     correo: "pont@loco.com",
     telefono: "6785"
 };
-d.insertarUsuario(pablo);
-
-console.log("insertamos un mensaje");
-d.enviarMensaje({id:1}, {id:2}, "cosos");
+let neku = {
+    nombre: "Neku",
+    correo:"neku@ascii.fdi",
+    telefono:"123456789"
+}
+//d.insertarUsuario(pablo);
+//d.insertarUsuario(neku);
+pablo.id=1;
+neku.id=2;
+//console.log("insertamos un mensaje:");
+//d.enviarMensaje(pablo,neku, "Pena");
+//console.log(`Bandeja de entrada:`)
+//d.bandejaEntrada(neku);
+console.log(`Búsqueda`);
+d.buscarUsuario('n')
