00b84f1e5e725d6a46e67639ee3f061441bb86b6
Alapont <pablodragon@gmail.com>
2018-11-14 20:54:49 +0100

    Preparación hecha (todo adaptado al nuevo formato chupiguay) Ahora empieza lo gordo

--- PracticaGuiadaP4/apps.js
+++ PracticaGuiadaP4/apps.js
@@ -6,10 +6,10 @@ const path = require("path");
 const mysql = require("mysql");
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require("fs");
 
 // Crear un servidor Express.js
 const app = express();
+app.set("view engine", "ejs"); //usamos ejs  agromenaguer y candemor
 
 // Crear un pool de conexiones a la base de datos de MySQL
 const pool = mysql.createPool(config.mysqlConfig);
@@ -17,6 +17,33 @@ const pool = mysql.createPool(config.mysqlConfig);
 // Crear una instancia de DAOTasks
 const daoT = new DAOTasks(pool);
 
+//Middlewares
+
+//Definición de middlewares
+function logger(request, response, next) {
+    console.log(`Recibida petición ${request.method} ` +
+        `en ${request.url} de ${request.ip}`);
+    // Saltar al siguiente middleware
+    next();
+}
+
+function error(request, response, next) {
+    response.status(404);
+    response.render("error", {
+        url: request.url
+    });
+};
+
+const ficherosEstaticos = path.join(__dirname, "public");
+
+//Lista de middlewares
+app.use(logger);
+app.use(express.static(ficherosEstaticos));
+
+
+
+
+app.use(error);
 // Arrancar el servidor
 app.listen(config.port, function(err) {
    if (err) {
