addbee0e0997135937e8b1534caf42207b30f0cc
Alapont <pablo.alapont@gmail.com>
2018-11-29 13:11:26 +0100

    Pasamos los datos. Pero sigue sin haberlogin

--- PracticaGuiadaP5/DAOUsers.js
+++ PracticaGuiadaP5/DAOUsers.js
@@ -11,12 +11,12 @@ class DAOUsers{
             if(err){
                 callback(`Error de conexion a la base de datos`);
             }else{
-                const sql= `SELECT email,user, password FROM user WHERE email= ? AND password = ?`;
+                const sql= `SELECT user FROM user WHERE email= ? AND password = ?`;
                 connection.query(sql, [email,password], function(err,resultado){
                     if(err){
                         callback(`Error de acceso a la base de datos`);
                     }else{
-                        callback(null,(resultado.length==1)?resultado[0].user:null);
+                        callback(null,(resultado.length==1)?resultado[0]:null);
                         //callback(null,resultado.length==1);
                     }
                 })
@@ -35,7 +35,7 @@ class DAOUsers{
                     if(err){
                         callback(`Error de acceso a la base de datos`);
                     }else{
-                        if(resultado.length==0){
+                        if(resultado.length!=1){
                             callback(`No existe el usuario`);
                         }else{
                             callback(null,resultado[0]);
