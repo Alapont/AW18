32f805873a4ffdd6ed97ae4fc265b3bb1fe8e01a
Alapont <pablodragon@gmail.com>
2018-12-09 18:12:03 +0100

    mensaje de error

--- FB/app.js
+++ FB/app.js
@@ -165,6 +165,8 @@ app.get('/', (request, response) => {
     response.redirect("/login");
 });
 
+
+//Si llego hasta aqui es un fallo
 app.use(error);
 // Arrancar el servidor
 app.listen(config.port, function (err) {
