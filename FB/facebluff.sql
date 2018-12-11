cc2ccbdfd8fca148b165af9e960dff97200fec49
Nerea <jguillen.nereajimenez@gmail.com>
2018-12-11 17:02:07 +0100

    new database

--- FB/facebluff.sql
+++ FB/facebluff.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-12-2018 a las 22:07:39
--- Versión del servidor: 10.1.29-MariaDB
--- Versión de PHP: 7.1.12
+-- Tiempo de generación: 11-12-2018 a las 17:01:13
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 7.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -46,69 +46,6 @@ INSERT INTO `amistad` (`Amigado`, `Amigador`, `Estado`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `preguntas`
---
-
-CREATE TABLE `preguntas` (
-  `id` int(10) NOT NULL,
-  `texto` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `preguntas`
---
-
-INSERT INTO `preguntas` (`id`, `texto`) VALUES
-(1, '¿En que año fue 1 + 1?'),
-(2, '¿Tu eres mas de ASCII o de ASCII?'),
-(3, '¿Qué llevas puesto?');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `respuestas`
---
-
-CREATE TABLE `respuestas` (
-  `idRespuesta` int(10) NOT NULL,
-  `idPregunta` int(10) NOT NULL,
-  `Texto` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `respuestas`
---
-
-INSERT INTO `respuestas` (`idRespuesta`, `idPregunta`, `Texto`) VALUES
-(1, 1, 'el fantástico Ralph'),
-(2, 1, 'al menos dos'),
-(3, 2, 'De ASCII'),
-(4, 2, 'de ascii, por supuesto');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `respuestausuario`
---
-
-CREATE TABLE `respuestausuario` (
-  `idPregunta` int(10) NOT NULL,
-  `idRespuesta` int(10) NOT NULL,
-  `usuario` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `respuestausuario`
---
-
-INSERT INTO `respuestausuario` (`idPregunta`, `idRespuesta`, `usuario`) VALUES
-(1, 1, 'pont@loco.es'),
-(1, 2, 'neku@ascii.com'),
-(2, 1, 'neku@ascii.com');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `sessions`
 --
 
@@ -123,8 +60,12 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('P765HTmbdQbWjtpsTG6crzPJeeNYRDAd', 1544467553, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"userName\":\"pont\",\"email\":\"pont@loco.es\",\"img\":\"diploascii.jpg\",\"nacimiento\":\"\",\"sexo\":\"\",\"puntos\":0}'),
-('ZrO9gMl3sR1mavf3a1I2SMynGr12waju', 1544465690, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"userName\":\"pont\",\"email\":\"pont@loco.es\",\"img\":\"diploascii.jpg\",\"nacimiento\":\"\",\"sexo\":\"\",\"puntos\":0,\"amigos\":[{\"userName\":\"nerea\",\"estado\":\"amigo\"},{\"userName\":\"Yago\",\"estado\":\"amigo\"}],\"solicitudes\":[{\"userName\":\"Pascal\",\"estado\":\"solicitud\"}]}');
+('EO8WNONKIreBCgt18H4Mr3ublFegxkfq', 1544563796, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"flashMsg\":[{\"location\":\"body\",\"param\":\"user\",\"msg\":\"El email no puede estar vacío\",\"value\":\"\"},{\"location\":\"body\",\"param\":\"password\",\"msg\":\"La contraseña no puede estar vacía\",\"value\":\"\"},{\"location\":\"body\",\"param\":\"userName\",\"msg\":\"El nombre de usuario no puede estar vacío\",\"value\":\"\"}]}'),
+('G4IVSrIE2heJ4I9D11OqlKhFXhhoFUpd', 1544605914, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"userName\":\"pont\",\"email\":\"pont@loco.es\",\"img\":\"diploascii.jpg\",\"nacimiento\":\"\",\"sexo\":\"\",\"puntos\":0}'),
+('MN3yR7iMnTJNCFRcMx2RofmGfi-q1A37', 1544602743, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"userName\":\"pont\",\"email\":\"pont@loco.es\",\"img\":\"diploascii.jpg\",\"nacimiento\":\"\",\"sexo\":\"\",\"puntos\":0,\"amigos\":[{\"userName\":\"nerea\",\"estado\":\"amigo\",\"img\":\"gato.jpg\",\"email\":\"neku@ascii.com\"},{\"userName\":\"Yago\",\"estado\":\"amigo\",\"img\":null,\"email\":\"yago@ascii.com\"}],\"solicitudes\":[{\"userName\":\"Pascal\",\"estado\":\"solicitud\",\"img\":null,\"email\":\"pascal@ascii.com\"}]}'),
+('MtUUoUedOq2wFYZjsCeemUtbIBd2iKN0', 1544630319, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"userName\":\"PontMola\",\"email\":\"pont@loco.es\",\"img\":\"\",\"nacimiento\":\"\",\"sexo\":\"male\",\"puntos\":0,\"amigos\":[{\"userName\":\"nerea\",\"estado\":\"amigo\",\"img\":\"gato.jpg\",\"email\":\"neku@ascii.com\"},{\"userName\":\"Yago\",\"estado\":\"amigo\",\"img\":null,\"email\":\"yago@ascii.com\"}],\"solicitudes\":[{\"userName\":\"Pascal\",\"estado\":\"solicitud\",\"img\":null,\"email\":\"pascal@ascii.com\"}]}'),
+('Y7VYVjLlbZYVxBK3NnDMWAatNorIhAcs', 1544602688, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"error\":\"usuario no encontrado\",\"userName\":\"pont\",\"email\":\"pont@loco.es\",\"img\":\"diploascii.jpg\",\"nacimiento\":\"\",\"sexo\":\"\",\"puntos\":0}'),
+('or39YzJu8AAT8Sy_q5mKfWmQsilW_Z_b', 1544605974, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"userName\":\"PontMola\",\"email\":\"pont@loco.es\",\"img\":\"\",\"nacimiento\":\"\",\"sexo\":\"male\",\"puntos\":0}');
 
 -- --------------------------------------------------------
 
@@ -135,7 +76,7 @@ INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
 CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `img` varchar(100) DEFAULT NULL,
+  `img` blob,
   `userName` varchar(100) NOT NULL,
   `sexo` varchar(15) NOT NULL,
   `nacimiento` varchar(10) NOT NULL,
@@ -147,10 +88,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`email`, `password`, `img`, `userName`, `sexo`, `nacimiento`, `puntos`) VALUES
-('neku@ascii.com', 'caraculo', 'gato.jpg', 'nerea', '', '', 0),
+('neku@ascii.com', 'caraculo', 0x6761746f2e6a7067, 'nerea', '', '', 0),
 ('Noel@ascii.com', 'caca', NULL, 'Noel', 'hombre', '19890615', 0),
 ('pascal@ascii.com', 'pascal', NULL, 'Pascal', '', '', 0),
-('pont@loco.es', 'kaka', 'diploascii.jpg', 'pont', '', '', 0),
+('pont@loco.es', 'kaka', '', 'PontMola', 'male', '', 0),
+('prueba@lolo.com', 'a', '', 'yolo', 'other', '2000-12-06', 0),
 ('yago@ascii.com', 'yago', NULL, 'Yago', '', '', 0);
 
 --
@@ -161,29 +103,7 @@ INSERT INTO `users` (`email`, `password`, `img`, `userName`, `sexo`, `nacimiento
 -- Indices de la tabla `amistad`
 --
 ALTER TABLE `amistad`
-  ADD PRIMARY KEY (`Amigado`,`Amigador`),
-  ADD KEY `Amigador` (`Amigador`);
-
---
--- Indices de la tabla `preguntas`
---
-ALTER TABLE `preguntas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `respuestas`
---
-ALTER TABLE `respuestas`
-  ADD PRIMARY KEY (`idRespuesta`,`idPregunta`),
-  ADD KEY `idPregunta` (`idPregunta`);
-
---
--- Indices de la tabla `respuestausuario`
---
-ALTER TABLE `respuestausuario`
-  ADD PRIMARY KEY (`idPregunta`,`idRespuesta`,`usuario`),
-  ADD KEY `idRespuesta` (`idRespuesta`),
-  ADD KEY `usuario` (`usuario`);
+  ADD PRIMARY KEY (`Amigado`,`Amigador`);
 
 --
 -- Indices de la tabla `sessions`
@@ -196,47 +116,6 @@ ALTER TABLE `sessions`
 --
 ALTER TABLE `users`
   ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `preguntas`
---
-ALTER TABLE `preguntas`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `respuestas`
---
-ALTER TABLE `respuestas`
-  MODIFY `idRespuesta` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `amistad`
---
-ALTER TABLE `amistad`
-  ADD CONSTRAINT `amistad_ibfk_1` FOREIGN KEY (`Amigado`) REFERENCES `users` (`email`),
-  ADD CONSTRAINT `amistad_ibfk_2` FOREIGN KEY (`Amigador`) REFERENCES `users` (`email`);
-
---
--- Filtros para la tabla `respuestas`
---
-ALTER TABLE `respuestas`
-  ADD CONSTRAINT `respuestas_ibfk_1` FOREIGN KEY (`idPregunta`) REFERENCES `preguntas` (`id`);
-
---
--- Filtros para la tabla `respuestausuario`
---
-ALTER TABLE `respuestausuario`
-  ADD CONSTRAINT `respuestausuario_ibfk_1` FOREIGN KEY (`idPregunta`) REFERENCES `preguntas` (`id`),
-  ADD CONSTRAINT `respuestausuario_ibfk_2` FOREIGN KEY (`idRespuesta`) REFERENCES `respuestas` (`idRespuesta`),
-  ADD CONSTRAINT `respuestausuario_ibfk_3` FOREIGN KEY (`usuario`) REFERENCES `users` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
