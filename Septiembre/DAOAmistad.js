f64c9943cc5cccad5a48c931e38a2635a60940a4
palapont <pablo.alapont@soprasteria.com>
2019-05-27 19:38:53 +0200

    La query de amistades

--- Septiembre/DAOAmistad.js
+++ Septiembre/DAOAmistad.js
@@ -5,8 +5,28 @@ class DAOAmistad {
         this._pool = pool;
         if (debug)
             console.log(pool);
-        this._estados = ["amigo", "solicitar","rechazar"];
+        this._estados = ["pendiente","aceptado","rechazado"];
     }
+    peticion(origen,destino,callback=test){
+        this._pool.getConnection(function (err, connection) {
+            if (err) {
+                callback(`Error de conexion a la base de datos`);
+            } else {
+                const sql = `INSERT INTO amistad (idOrigen, idDestino,estado) VALUES (?,?,?);`
+                connection.query(sql, [origen,destino,"pendiente"], function (err, resultado) {
+                    if (err) {
+                        callback(`Error de acceso a la base de datos`);
+                    } else {
+                        
+                         callback(null, resultado);
+                         
+                    }
+                });
+            }
+            connection.release();
+        });
+    }
+
     setAmistad(amigado, amigador,estado, callback = test) {
         this._pool.getConnection(function (err, connection) {
             if (err) {
@@ -46,9 +66,32 @@ class DAOAmistad {
                 }
                 connection.release();
             });
-        }
+    }
     
-
+    getAmistades(user,callback=test){
+        this._pool.getConnection((err, connection)=>{
+            if (err) {
+                callback(`Error de conexion a la base de datos`);
+            } else {
+                const sql = 
+                `SELECT U1.ID, U1.UserName,U1.points,U1.img
+                FROM amistad A1 JOIN users U1 ON A1.IdOrigen=U1.ID 
+                WHERE A1.IdDestino=? AND A1.estado = 'aceptado' AND U1.activo=1
+                UNION 
+                SELECT U2.ID, U2.UserName,U2.points,U2.img
+                FROM amistad A2 JOIN users U2 ON A2.IdDestino=U2.ID 
+                WHERE A2.IdOrigen = ? AND A2.estado = 'aceptado' AND U2.activo=1`;
+                connection.query(sql, [user, user], function (err, resultado) {
+                    if (err) {
+                        callback(`Error de acceso a la base de datos`);
+                    } else {
+                        callback(null, resultado);
+                    }
+                    connection.release();
+                });  
+            }
+        });
+    }
     getAmigos(user, callback = test) {
         this._pool.getConnection((err, connection)=>{
             if (err) {
